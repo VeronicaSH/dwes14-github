6aec9d32881fafc5d947c314a25726d0c7c96144
Alumno <Alumno@W7Bsica>
2017-11-08 12:23:02 +0100

    12:22

--- Validacion.php/index2.php
+++ Validacion.php/index2.php
@@ -1,20 +1,44 @@
 <?php
 include ("datos.php");
-$error=false;
-$pass=false;
-if(!isset($_POST['enviar']) || $error=true){
-    ?>
+$error_tel="";
+$error_mail="";
+$error_pass="";
+$error_fecha="";
+if(isset($_POST['enviar'])){
+    $error=false;
+    
+    if (!validar_tel($_POST["telefono"])){
+        $error=true;
+        $error_tel;
+        
+    }
+    if (!validar_pass($_POST["pass"])){
+        $error_pass;
+        $error=true;
+    }
+    if (!validar_fecha($_POST["fecha"])){
+        $error=true;
+        $error_mail;
+    }
+    if (!validar_email($_POST["email"])){
+        $error=true;
+        $error_mail;
+    }
+}
+if(!isset($_POST['enviar'])||$error==true){
+
+?>
 <div id="cuerpo">
 
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
 Nombre: <input type="text" name="nombre" value="nombre" required>
 Apellidos: <input type="text" name="apellidos" value="apellidos" required> 
-Contraseña: <input type="password" name="pass" value="contraseña" required>
-Email: <input type="text" name="email" value="email" required>
+Contraseña: <input type="password" name="pass" value="contraseña" required> <?php $error_pass="contraseña incorrecta"?>
+Email: <input type="text" name="email" value="email" required><?php $error_mail="mail incorrecto"?>
 
-Fecha: <input type="date" name="fecha" required>
+Fecha: <input type="date" name="fecha" required><?php $error_fecha="fecha no valida"?>
 Direccion: <input type="text" name="direccion" value="direccion" required>
-Telefono: <input type="text" name="telefono" value="telefono" required>
+Telefono: <input type="text" name="telefono" value="telefono" required><?php $error_tel="telefono incorrecto"?>
 Ciclo: <select id="ciclos">
 	<option value="ASIR" selected="selected">ASIR </option>
 	<option value="DAW"> DAW </option>
@@ -29,33 +53,8 @@ Ciclo: <select id="ciclos">
 
 
 }else{
-    if (!validar_tel($_POST["telefono"])){
-        $error=true;
-    }
-    
-    
-    if (!validar_pass($_POST["pass"])){
-      
-        $error=true;
-    }else{
-       
-       
-    }
-    
-    
-    if (!validar_fecha($_POST["fecha"])){
-        $error=true;
-    }
-    if (!validar_email($_POST["email"])){
-        $error=true;
+    echo "Formulario enviado";
 }
-
-}
-        }elseif(!$error && (isset($_POST["enviar"]))) {
-            echo "Formulario enviado";
-        }
-       
-       
        ?>
 
         
