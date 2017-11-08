c0975d432ad350a5e45633eec370c62c6cd234f0
Alumno <Alumno@W7Bsica>
2017-11-08 11:55:52 +0100

    11:55

--- Validacion.php/index2.php
+++ Validacion.php/index2.php
@@ -2,7 +2,7 @@
 include ("datos.php");
 $error=false;
 $pass=false;
-if(!isset($_POST['enviar'])){
+if(!isset($_POST['enviar']) || $error=true){
     ?>
 <div id="cuerpo">
 
@@ -51,33 +51,6 @@ Ciclo: <select id="ciclos">
 }
 
 }
-if($error ){
-   ?> <div id="cuerpo">
-    
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-        Nombre: <input type="text" name="nombre" value="nombre" required>
-        Apellidos: <input type="text" name="apellidos" value="apellidos" required>
-        Contraseña: <input type="password" name="pass" value="contraseña" required>
-        <?php  if($pass){echo"la contraseña debe tener entre 7 y 15 caracteres alfanumericos,
-        incluyendo una mayuscula";}  ?>
-        Email: <input type="text" name="email" value="email" required>
-        <?php  echo"Ese campo no corresponde con un email";  ?>
-       	Fecha: <input type="date" name="fecha" required>
-         <?php  echo"No corresponde a una fecha";  ?>
-        Direccion: <input type="text" name="direccion" value="direccion" required>
-        Telefono: <input type="text" name="telefono" value="telefono" required>
-         <?php  echo"No corresponde con un telefono";  ?>
-        Ciclo: <select id="ciclos">
-        	<option value="ASIR" selected="selected">ASIR </option>
-        	<option value="DAW"> DAW </option>
-        	<option value="DAM"> DAM </option>
-        </select> 
-        <input type="submit" name="enviar">
-        </form>
-        
-        </div>
-          
-   <?php  
         }elseif(!$error && (isset($_POST["enviar"]))) {
             echo "Formulario enviado";
         }
