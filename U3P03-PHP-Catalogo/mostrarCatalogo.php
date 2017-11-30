3283d10a3a68009650ad03a35a94e78b29640f2c
Alumno <Alumno@W7Bsica>
2017-11-30 14:13:17 +0100

    14:13

--- U3P03-PHP-Catalogo/mostrarCatalogo.php
+++ U3P03-PHP-Catalogo/mostrarCatalogo.php
@@ -16,41 +16,66 @@ $conexion->query("SET NAMES 'UTF8'");
 if ($conexion->connect_errno) {
     echo "<p>Error al establecer la conexión (" . $conexion->connect_errno . ") " . $conexion->connect_error . "</p>";
 }
-?>
-<table style='border:0'>
-<tr style='background-color:lightblue'>
-<th>Nombre <a href="mostrarCatalogo.php?order=1&<?php if($autor!='') echo 'autor='.$autor; ?>"> &#9650</a> 
-<a href="mostrarCatalogo.php?order=0&<?php if($autor!='')echo 'autor='.$autor ;?>"> &#9660</a></th></th>
-<th>Autor<a href="mostrarCatalogo.php?order=1&<?php if($autor!='') echo 'autor='.$autor; ?>"> &#9650</a> 
-<a href="mostrarCatalogo.php?order=0&<?php if($autor!='')echo 'autor='.$autor ;?>"> &#9660</a></th></th>
-</tr>
-<?php
-$order='';
-$numero='';
-if(isset($_REQUEST["order"]) && $_REQUEST["order"]==1){
-    $order='ASC';
-    $numero=1;
-}else{
-    $order='DESC';
-    $numero=0;
-}
 $autor='';
 if(isset($_REQUEST["autor"])){
-    $autor=$_REQUEST["autor"];
-    if (isset($_SESSION['autor'])){
-        $autor=$_SESSION["autor"];}
-        $resultado = $conexion->query('SELECT * FROM obra,autor WHERE obra.autor=autor.idAutor AND autor.Nombre="'.$autor.'" ORDER BY obra.Nombre '. $order);
-}else{
-    $resultado = $conexion->query('SELECT * FROM obra,autor WHERE obra.autor=autor.idAutor ORDER BY autor.Nombre '. $order);
+    $autor=" AND autor='$_REQUEST[autor]'";
+    echo "<a href='mostrarCatalogo.php'>Volver</a>";
+    
 }
+$order='';
+
+?>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+<input type="text" name="busqueda">
+<input type="submit"  name="enviar">
+</form>
+<table style='border:0'>
+<tr style='background-color:lightblue'>
+<th>Nombre <a href="mostrarCatalogo.php?order_n=1"> &#9650</a> 
+<a href="mostrarCatalogo.php?order_n=0"> &#9660</a></th></th>
+<th>Autor<a href="mostrarCatalogo.php?order_a=1&<?php if($autor!='') echo 'autor='.$autor; ?>"> &#9650</a> 
+<a href="mostrarCatalogo.php?order_a=0&<?php if($autor!='') echo 'autor='.$autor; ?>"> &#9660</a></th></th>
+
+</tr>
+<?php
+
+//ordenacion 
+if(isset($_POST["enviar"])){
+    $buscar=$_REQUEST["busqueda"];
+    $resultado= $conexion -> query("SELECT * FROM obra,autor WHERE  autor.idAutor=obra.autor AND autor.nombre='$buscar'");
+}
+elseif(isset($_REQUEST["order_a"]) && $_REQUEST["order_a"]==1){
+    $resultado = $conexion->query("SELECT * FROM obra,autor WHERE   autor.idAutor=obra.autor $autor ORDER BY obra.Nombre ASC");
+    
+    
+}elseif(isset($_REQUEST["order_a"]) && $_REQUEST["order_a"]==0){
+    $resultado = $conexion->query("SELECT * FROM obra,autor WHERE  autor.idAutor=obra.autor $autor ORDER BY obra.Nombre DESC");;
+
+    
+}elseif(isset($_REQUEST["order_n"]) && $_REQUEST["order_n"]==1){
+    $resultado = $conexion->query("SELECT * FROM obra,autor WHERE  autor.idAutor=obra.autor $autor ORDER BY obra.Nombre ASC");
+    
+    
+}elseif (isset($_REQUEST["order_n"]) && $_REQUEST["order_n"]==0){
+    $resultado = $conexion->query("SELECT * FROM obra,autor WHERE autor.idAutor=obra.autor $autor ORDER BY obra.Nombre DESC");;
+   
+    
+}else{
 //creacion de la tabla
-$resultado=$conexion -> query("SELECT * from obra,autor WHERE autor.idAutor=obra.autor");
+$resultado=$conexion -> query("SELECT * from obra,autor WHERE autor.idAutor=obra.autor $autor");
+}
+
 if($resultado->num_rows === 0) echo "<p>No hay datos en la Base</p>";
 while ($obra = $resultado->fetch_assoc() ){
     echo "<tr bgcolor='lightgreen'>";
     echo "<td> <a href='mostrarObra.php?idJuego=$obra[Nombre]'> $obra[Nombre]</td>\n";
-    echo "<td> $obra[nombre]</td>\n";
+    echo "<td> <a href='mostrarCatalogo.php?autor=$obra[idAutor]' > $obra[nombre]</td>\n";
     echo "</tr>";
     
 }
+
 ?>
+</table>
+<a href="mostrarCatalogo.php">Eliminar filtros</a>
+
+
