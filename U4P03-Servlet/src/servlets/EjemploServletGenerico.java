eee6850c4a51d78734e169de0bc93a9a415a33aa
Alumno <Alumno@W7Bsica>
2018-01-11 14:25:01 +0100

    new

--- U4P03-Servlet/src/servlets/EjemploServletGenerico.java
+++ U4P03-Servlet/src/servlets/EjemploServletGenerico.java
@@ -12,8 +12,10 @@ public class EjemploServletGenerico extends GenericServlet {
 
   public void service(ServletRequest req, ServletResponse res) throws IOException {
     log("Petición recibida desde " + req.getRemoteAddr());
-    res.setContentType("text/plain");
+    res.setContentType("text/html");
     res.getWriter().println("Mensaje desde el servlet genérico de ejemplo");
+    
     res.getWriter().close();
   }
+ 
 }
