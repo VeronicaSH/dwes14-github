57b2d8b071a71c8db198a45864b1152b348109ef
Alumno <Alumno@W7Bsica>
2018-02-08 13:21:38 +0100

    practica 11 terminada

--- U4P11-Java-Filtros/src/filters/WrapFilter.java
+++ U4P11-Java-Filtros/src/filters/WrapFilter.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -28,8 +29,11 @@ public class WrapFilter implements Filter {
 	
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		response.setContentType("text/html;UTF-8");
+		ServletContext contexto=request.getServletContext();
 		PrintWriter out = response.getWriter(); 
-		out.println("<html><body><center>Cabecera<hr />"); 
+		out.println("<html><head><title>Visitas:" + contexto.getAttribute("contador") +"</title></head><body><center>Cabecera<hr />");
+		
+		
 		chain.doFilter(request, response);
 		out.println("<hr /><center>Page footer</body></html>"); 
 	}
