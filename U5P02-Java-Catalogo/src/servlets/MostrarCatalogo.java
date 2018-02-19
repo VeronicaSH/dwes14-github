f016780a31425148f9832a0ca308c033cfe16d95
Alumno <Alumno@W7Bsica>
2018-02-19 11:15:57 +0100

    practica 2 tema 5 (2.2 mod)

--- U5P02-Java-Catalogo/src/servlets/MostrarCatalogo.java
+++ U5P02-Java-Catalogo/src/servlets/MostrarCatalogo.java
@@ -1,6 +1,13 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,7 +27,59 @@ public class MostrarCatalogo extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		ServletContext contexto = getServletContext();
+		response.setContentType("text/html;UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><meta charset='UTF-8'/></head><body>");
+		//conexion con la BBDD
+		Connection conn = null;
+		Statement sentencia = null;
+		try {
+		  // Paso 1: Cargar el driver JDBC.
+		  Class.forName("org.mariadb.jdbc.Driver").newInstance();
+
+		  // Paso 2: Conectarse a la Base de Datos utilizando la clase Connection
+		  String userName = "alumno";
+		  String password = "alumno";
+		  String url = "jdbc:mariadb://localhost/catalogo14";
+		  conn = DriverManager.getConnection(url, userName, password);
+
+		  // Paso 3: Crear sentencias SQL, utilizando objetos de tipo Statement
+		  sentencia = conn.createStatement();
+
+		  // Paso 4: Ejecutar la sentencia SQL a través de los objetos Statement
+		  String consulta = "SELECT * from obra";
+		  ResultSet rset = sentencia.executeQuery(consulta);
+		  //detectar si no hay resultados
+		  if (!rset.isBeforeFirst() ) {    
+			    out.println("<h3>No hay resultados</p>");
+			}
+		  //variable imagen
+		  String img="./img/";
+		  // Paso 5: Mostrar resultados
+		  out.println("<table>");
+		  out.println("<tr>" + "<td>idJuego</td>"+ "<td>Nombre</td>" + "<td>Genero</td>" +  "<td>Consola</td>"+  "<td>Autor</td>"+  "<td>Imagen</td>"+"</tr>" );
+		  while (rset.next()) {
+			
+		    out.println("<tr>" + "<td>" + rset.getString("idJuego")+"</td>"+
+		    "<td>"+ rset.getString("Nombre") + "</td>" +
+		    "<td>"+ rset.getString("genero") + "</td>" +
+		    "<td>"+ rset.getString("consola") + "</td>" +
+		    "<td>"+ rset.getString("autor") + "</td>" +
+		    "<td> <img src='  "+img+ rset.getString("Imagen") +"   ' width='100px'></td></tr>");
+		  }
+		  out.println("</table>");
+
+		  // Paso 6: Desconexión
+		  if (sentencia != null)
+		    sentencia.close();
+		  if (conn != null)
+		    conn.close();
+		} catch (Exception e) {
+		  e.printStackTrace();
+		}
+		out.println("</body></html>");
+	
 	}
 
 	
