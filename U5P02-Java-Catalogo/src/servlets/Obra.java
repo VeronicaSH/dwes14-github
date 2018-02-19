903e13d72cfeec6065a36163d3a4b6a809822026
Alumno <Alumno@W7Bsica>
2018-02-19 11:06:18 +0100

    Practica 2 tema 5 (2.2)

---
+++ U5P02-Java-Catalogo/src/servlets/Obra.java
@@ -0,0 +1,29 @@
+package servlets;
+
+public class Obra {
+	public int idJuego;
+    public String nombre;
+    public String genero;
+    public String consola;
+    public String autor;
+    public String imagen;
+    
+    public int getIdJuego(){
+        return idJuego;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getGenero(){
+        return genero;
+    }
+    public String getConsola(){
+        return consola;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public String getImagen(){
+        return imagen;
+    }
+}
