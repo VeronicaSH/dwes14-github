83f77bae071136253b0fd9f6c13d2a7f406f755f
Alumno <Alumno@W7Bsica>
2018-02-20 13:59:10 +0100

    practica 2 tema 5 (2.3.9)

--- U5P02-Java-Catalogo/src/servlets/MostrarObra.java
+++ U5P02-Java-Catalogo/src/servlets/MostrarObra.java
@@ -1,6 +1,13 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,9 +27,66 @@ public class MostrarObra extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ServletContext contexto = getServletContext();
+		response.setContentType("text/html;UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><meta charset='UTF-8'/></head><body>");
+		//conexion con la BBDD
+		Connection conn = null;
+		Statement sentencia = null;
+		try {
+		  // Paso 1: Cargar el driver JDBC.
+		  Class.forName("org.mariadb.jdbc.Driver").newInstance();
+
+		  // Paso 2: Conectarse a la Base de Datos utilizando la clase Connection
+		  String userName = "alumno";
+		  String password = "alumno";
+		  String url = "jdbc:mariadb://localhost/catalogo14";
+		  conn = DriverManager.getConnection(url, userName, password);
+
+		  // Paso 3: Crear sentencias SQL, utilizando objetos de tipo Statement
+		  sentencia = conn.createStatement();
+
+		  // Paso 4: Ejecutar la sentencia SQL a través de los objetos Statement
+		  String consulta = "SELECT * from obra,autor WHERE autor.idAutor=obra.autor";
+		  ResultSet rset = sentencia.executeQuery(consulta);
+		  //detectar si no hay resultados
+		  if (!rset.isBeforeFirst() ) {    
+			    out.println("<h3>No hay resultados</p>");
+			}
+		  //variable imagen
+		  String img="./img/";
+		  // Paso 5: Mostrar resultados
+		  out.println("<table>");
+		  out.println("<tr>" + "<td>IdJuego</td>" + "<td>Nombre</td>" + "<td>Genero</td>"+ "<td>Consola</td>"  + "<td>Autor</td>"+  "<td>Imagen</td>"+ "</tr>"  );
+		  while (rset.next()) {
+			  //objeto de la clase obra
+			Obra o=new Obra(rset.getString("idJuego"), rset.getString("Nombre"), rset.getString("genero"), rset.getString("consola"), rset.getString("autor"), rset.getString("Imagen"), rset.getString("nombre_autor"));
+			out.println("<tr>" + 
+			//llamada a los get de la clase obra para recuperar los datos
+			"<td>"+ o.getIdJuego() + "</td>" +
+		    "<td>"+ o.getNombre() + "</td>" +
+		    "<td>"+ o.getGenero() + "</td>" +
+		    "<td>"+ o.getConsola() + "</td>" +
+		    "<td>"+ o.getNombreAutor() + "</td>" +
+		    "<td> <img src='"+img+ o.getImagen() +"' width='100px'></td></tr>");
+		    
+		  }
+		  out.println("</table>");
+
+		  // Paso 6: Desconexión
+		  if (sentencia != null)
+		    sentencia.close();
+		  if (conn != null)
+		    conn.close();
+		} catch (Exception e) {
+		  e.printStackTrace();
+		}
+		out.println("</body></html>");
+	
 	}
+		
+	
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
